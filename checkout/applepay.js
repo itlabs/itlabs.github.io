9fd3f67d9d8d13f67647e26c200f60249898715d
Jacky Pan <jacky.pan@yahoo.com>
2023-06-05 16:00:29 -0400

    test

--- checkout/applepay.js
+++ checkout/applepay.js
@@ -1,31 +1,46 @@
 var MECHAT_ID = "merchant.test.example.com";
-var BACKEND_URL_VALIDATE_SESSION =  window.location.href + "validateSession";
+var BACKEND_URL_VALIDATE_SESSION = window.location.href + "validateSession";
 var BACKEND_URL_PAY = window.location.href + "pay";
 
 var appleButton = document.querySelection(".apple-pay-button");
 
 //check if apple pay is available
 if (window.ApplePaySession && ApplePaySession.canMakePaymentsWithActiveCard(MECHAT_ID) {
-	appleButton.style.display = "block";
+    appleButton.style.display = "block";
 }
 
-appleButton.addEventListener("click", function() {
-	var applePaySession = new ApplePaySession(6, {
-		countryCode: "US",
-		currencyCode: "USD",
-		supportedNetworks: ["visa", "masterCard", "amex", "discover"],
-		merchantCapabilities: ["supports3DS],
-		total: {label: "Amazing Shop", amount: "10.00"}
-	});
-	
-	applePaySession.begin();
-	
-	applePaySession.onvalidateMerchant = function(event) {
+appleButton.addEventListener("click", function () {
+
+    var request = {
+        countryCode: 'US',
+        currencyCode: 'USD',
+        supportedNetworks: ['visa', 'masterCard', 'amex', 'discover'],
+        merchantCapabilities: ['supports3DS'],
+        total: {
+            label: 'Your Merchant Name',
+            amount: '10.00'
+        },
+    }
+    var session = new ApplePaySession(3, request);
 		
-	};
-	
-	applePaySession.onpaymentauthorized = function(event) {
-		
-	};
-	
+	session.begin();
+
+        var applePaySession = new ApplePaySession(6, {
+            countryCode: "US",
+            currencyCode: "USD",
+            supportedNetworks: ["visa", "masterCard", "amex", "discover"],
+            merchantCapabilities: ["supports3DS],
+                		total: {label: " Amazing Shop ", amount: " 10.00 "}
+                	});
+
+                	//applePaySession.begin();
+
+                	//applePaySession.onvalidateMerchant = function(event) {
+
+                	//};
+
+                	//applePaySession.onpaymentauthorized = function(event) {
+
+                	//};
+
 });
