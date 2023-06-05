ea6b6228c5dc023819513dfaeb8f69e48eeef1a8
Jacky Pan <jacky.pan@yahoo.com>
2023-06-05 15:51:30 -0400

    apple pay creates a session

---
+++ checkout/applepay.js
@@ -0,0 +1,31 @@
+var MECHAT_ID = "merchant.test.example.com";
+var BACKEND_URL_VALIDATE_SESSION =  window.location.href + "validateSession";
+var BACKEND_URL_PAY = window.location.href + "pay";
+
+var appleButton = document.querySelection(".apple-pay-button");
+
+//check if apple pay is available
+if (window.ApplePaySession && ApplePaySession.canMakePaymentsWithActiveCard(MECHAT_ID) {
+	appleButton.style.display = "block";
+}
+
+appleButton.addEventListener("click", function() {
+	var applePaySession = new ApplePaySession(6, {
+		countryCode: "US",
+		currencyCode: "USD",
+		supportedNetworks: ["visa", "masterCard", "amex", "discover"],
+		merchantCapabilities: ["supports3DS],
+		total: {label: "Amazing Shop", amount: "10.00"}
+	});
+	
+	applePaySession.begin();
+	
+	applePaySession.onvalidateMerchant = function(event) {
+		
+	};
+	
+	applePaySession.onpaymentauthorized = function(event) {
+		
+	};
+	
+});
